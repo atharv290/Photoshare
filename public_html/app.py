fc94cef54c00cfe2a98be1f01913f44cbcf11645
Atharva joshi <121710360+atharv290@users.noreply.github.com>
2025-05-13 20:18:26 +0530

    Update app.py

--- public_html/app.py
+++ public_html/app.py
@@ -8,7 +8,7 @@ from werkzeug.utils import secure_filename
 import random
 import datetime
 import string
-import numpy as np
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
